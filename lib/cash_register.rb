fb446cdf606bfb1d5d84faec817640fa84502315
Ashwin Kannan <classifieddatabank@Ashwins-MacBook-Pro.local>
2019-08-27 17:20:18 -0500

    lab complete

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+class CashRegister
+    attr_accessor :total, :discount, :prev_total
+    attr_reader :items
 
+    def initialize(discount=nil)
+        @total = 0
+        @prev_total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(item, price, quantity=1)
+        self.prev_total = self.total
+        self.total += price * quantity
+        quantity.times {
+            self.items.push(item)
+        }
+    end
+
+    def apply_discount
+        if discount
+            self.total = (self.total * (1 - self.discount/100.0)).round
+            "After the discount, the total comes to $#{self.total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        self.items.pop
+        self.total = self.prev_total
+    end
+end
